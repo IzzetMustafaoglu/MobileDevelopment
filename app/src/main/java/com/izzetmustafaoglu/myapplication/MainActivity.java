acd6c9569eb7332ad6f7bd82bca75a4145b39615
Conders Club <izzetmustafaolgu00@gmail.com>
2023-05-24 11:28:06 +0300

    Aktivite Yaşam Döngüsü

---
+++ app/src/main/java/com/izzetmustafaoglu/myapplication/MainActivity.java
@@ -0,0 +1,37 @@
+package com.izzetmustafaoglu.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("on create called");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        System.out.println("on resume called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        System.out.println("on stop called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        System.out.println("on pause called");
+    }
+}
